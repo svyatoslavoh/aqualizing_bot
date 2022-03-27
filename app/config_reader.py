aeb4e6fad22bf2d3635468980fe6ca79fc73a7f2
svyatoslavoh <alegkonin@gmail.com>
2022-03-27 23:32:36 +0300

    fix

--- app/config_reader.py
+++ app/config_reader.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 class TgBot:
     token: str
     admin_id: int
+    conf_url: str
 
 
 @dataclass
@@ -22,6 +23,7 @@ def load_config(path: str):
     return Config(
         tg_bot=TgBot(
             token=tg_bot["token"],
-            admin_id=int(tg_bot["admin_id"])
+            admin_id=int(tg_bot["admin_id"]),
+            conf_url=tg_bot["conf_url"]
         )
     )
